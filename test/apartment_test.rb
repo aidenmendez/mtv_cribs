dc33f3ac6a4db396c4b22adee66f6f7755247f61
Aiden Mendez <aidenmendez@gmail.com>
2020-10-20 09:36:19 -0600

    Test add_renter adds renter object to apartment object

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -1,21 +1,32 @@
 require 'Minitest/autorun'
 require 'Minitest/pride'
 require './lib/apartment'
+require './lib/renter'
 
 class ApartmentTest < Minitest::Test
-  def test_it_exists_and_has_attributes
-    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  def setup
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @renter1 = Renter.new("Jessie")
+  end
 
-    assert_instance_of Apartment, apartment
-    assert_equal "A1", unit1.number
-    assert_equal 1200, unit1.monthly_rent
-    assert_equal 1, unit1.bathrooms
-    assert_equal 1, unit1.bedrooms
+  def test_it_exists_and_has_attributes
+
+    assert_instance_of Apartment, @unit1
+    assert_equal "A1", @unit1.number
+    assert_equal 1200, @unit1.monthly_rent
+    assert_equal 1, @unit1.bathrooms
+    assert_equal 1, @unit1.bedrooms
   end
 
   def test_apartment_is_empty_by_default
-    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_nil @unit1.renter
+  end
 
-    assert_nil unit1.renter
+  def test_apartment_can_add_renter
+    assert_nil @unit1.renter
+
+    @unit1.add_renter(@renter1)
+
+    assert_equal @renter1, @unit1.renter
   end
 end
