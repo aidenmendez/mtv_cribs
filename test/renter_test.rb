8dc2386b791d90f850bcffb0a98c8195c08f71fc
Aiden Mendez <aidenmendez@gmail.com>
2020-10-20 09:17:45 -0600

    Add assert_instance_of method to renter test

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -6,6 +6,7 @@ class RenterTest < Minitest::Test
   def test_renter_exists_and_has_name
     renter = Renter.new("Jessie")
 
+    assert_instance_of Renter, renter
     assert_equal "Jessie", renter.name
   end
 end
