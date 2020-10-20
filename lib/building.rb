d0ca233f20dd2dbc1aebef87e027f82c8e419163
Aiden Mendez <aidenmendez@gmail.com>
2020-10-20 10:04:39 -0600

    Create average_rent method for building class

--- lib/building.rb
+++ lib/building.rb
@@ -13,4 +13,13 @@ class Building
   def add_renter(renter)
     @renters << renter.name
   end
+
+  def average_rent
+    total_rent = 0
+    units.each do |unit|
+      total_rent += unit.monthly_rent.to_f
+    end
+
+    total_rent/ units.length
+  end
 end
