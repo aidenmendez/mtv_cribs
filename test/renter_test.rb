6bedb514cc75c798d366e8fcbbf506c3c94243b1
Aiden Mendez <aidenmendez@gmail.com>
2020-10-20 09:08:43 -0600

    Create renter class and pass in name attribute

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -3,5 +3,9 @@ require 'Minitest/pride'
 require './lib/renter'
 
 class RenterTest < Minitest::Test
-  
+  def test_renter_exists_and_has_name
+    renter = Renter.new("Jessie")
+
+    assert_equal "Jessie", renter.name
+  end
 end
