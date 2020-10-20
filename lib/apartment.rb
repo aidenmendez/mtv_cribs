f8619053e974bfdbd7d7686af4fb1a55eda24fc1
Aiden Mendez <aidenmendez@gmail.com>
2020-10-20 09:37:13 -0600

    Fix circular reference error in initialization

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,11 +1,13 @@
 class Apartment
   attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
 
-  def initialize(number: number, monthly_rent: monthly_rent, bathrooms: bathrooms, bedrooms: bedrooms)
-    @number = number
-    @monthly_rent = monthly_rent
-    @bathrooms = bathrooms
-    @bedrooms = bedrooms
+  def initialize(apt_attr)
+    @number = apt_attr[:number]
+    @monthly_rent = apt_attr[:monthly_rent]
+    @bathrooms = apt_attr[:bathrooms]
+    @bedrooms = apt_attr[:bedrooms]
     @renter = nil
   end
+
+  
 end
