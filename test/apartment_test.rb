daf2d67f264bf3d542effb387f164507681ca755
Aiden Mendez <aidenmendez@gmail.com>
2020-10-20 09:04:44 -0600

    Create apartment and renter test files and set them up

---
+++ test/apartment_test.rb
@@ -0,0 +1,7 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/apartment'
+
+class ApartmentTest < Minitest::Test
+  
+end
---
+++ test/renter_test.rb
@@ -0,0 +1,7 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/renter'
+
+class RenterTest < Minitest::Test
+  
+end
